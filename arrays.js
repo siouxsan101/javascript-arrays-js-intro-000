31954a8b568009dff8fca23df74c23eb5b11dedb
susan hofmann <susanhofmannnyc@icloud.com>
2020-06-08 14:51:49 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,8 +1,7 @@
 var chocolateBars = ["snickers", "hundred grand", "kit kat", "skittles"]
 
 function addElementToBeginningOfArray(array,element) {
-  [element,...array];
-  return array; 
+  return [element, ...array];
   }
 
 function destructivelyAddElementToBeginningOfArray(array,element) {
