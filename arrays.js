433604958810ce411eba895083c0ff43a9301fda
susan hofmann <susanhofmannnyc@icloud.com>
2020-06-08 14:27:52 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,44 @@
+var chocolateBars = ["snickers", "hundred grand", "kit kat", "skittles"]
+
+function addElementToBeginningOfArray(array,element) {
+[element,...array];
+return array;
+  }
+
+function destructivelyAddElementToBeginningOfArray(array,element) {
+  array.unshift(element);
+  return array;
+}
+
+function addElementToEndOfArray(array,element) {
+  array.slice(-1);
+  return array;
+}
+
+function destructivelyAddElementToEndOfArray(array,element){
+  array.push(element);
+  return array;
+}
+
+function accessElementInArray(array,index) {
+  var element = (array[index]);
+  return element;
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array,element){
+  array.shift(element);
+}
+function removeElementFromBeginningOfArray(array,element){
+  array.slice(0);
+
+}
+
+function destructivelyRemoveElementFromEndOfArray(array,element){
+  array.pop();
+  return array;
+}
+
+function removeElementFromEndOfArray(array,element) {
+  array.slice(0,array.length-1);
+  return array;
+}
